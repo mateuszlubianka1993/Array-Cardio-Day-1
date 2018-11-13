ef277277574c5f176e7534ab3dce21b381b969eb
Mateusz Łubianka <lubiankamateusz@gmail.com>
2018-11-13 23:28:44 +0100

    add data

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const inventors = [
+    { first: 'Albert', last: 'Einstein', year: 1879, passed: 1955 },
+    { first: 'Osaac', last: 'Newton', year: 1643, passed: 1777 },
+    { first: 'Galileo', last: 'Galilei', year: 1564, passed: 1642 },
+    { first: 'Marie', last: 'Curie', year: 1867, passed: 1934 },
+    { first: 'Johannes', last: 'Kepler', year: 1571, passed: 1930 },
+    { first: 'Nicolaus', last: 'Copernicus', year: 1473, passed: 1543 },
+    { first: 'Max', last: 'Planc', year: 1858, passed: 1947 },
+];
+
+const people = ['Beck, Glenn', 'Becker, Carl', 'Beckett, Samuel', 'Beddoes, Mick', 'Beecher, Henry', 'Beethoven, Ludwig', 'Bellow, Saul', 'Benn, Tonny', 'Benson, Leana', 'Berlin, Irving', 'Bergman, Ingmar', 'Berry, Halle', 'Bevan, Aneurim', 'Bidem, Joseph', 'Black, Elk', 'Blair, Robert', 'Blair, Tony', 'Blake, William'];
